fdb61ebbb1707cae9924ee498a984642f1daa8bb
Vivek Sinha <Vivek.Sinha@nice.com>
2018-03-13 15:02:07 +0530

    ng5 commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ import { DataService } from '../data.service';
         query(':enter',stagger('300ms',[
           animate('.6s ease-in', keyframes([
             style({opacity:0, transform:'translateY(-75%)',offset:0}),
-            style({opacity:.5, transform:'translateY(35px)',offset:.3}),
+            style({opacity:.5, transform:'translateY(2px)',offset:.3}),
             style({opacity:1, transform:'translateY(0)',offset:1}),
           ]))
         ]),{optional:true}),
